01f400bb9c14e927f1a05fddaf18ae594402de93
Oluwamuyiwa123 <chefmuyi@gmail.com>
2025-04-17 12:29:12 +0100

    updated

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,5 +1,21 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "this" {
-  ami                    = var.ami_id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_group_ids
--- provider.tf
+++ provider.tf
@@ -14,101 +14,101 @@ provider "aws" {
   region = "us-west-2"  
 }
 
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+# # VPC
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
 
-  tags = {
-    Name = "muyiwa-vpc"
-  }
-}
+#   tags = {
+#     Name = "muyiwa-vpc"
+#   }
+# }
 
-# Subnet
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2b" 
-  map_public_ip_on_launch = true
+# # Subnet
+# resource "aws_subnet" "public" {
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = "10.0.1.0/24"
+#   availability_zone = "us-west-2b" 
+#   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "muyiwa-subnet"
-  }
-}
+#   tags = {
+#     Name = "muyiwa-subnet"
+#   }
+# }
 
-# Internet Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+# # Internet Gateway
+# resource "aws_internet_gateway" "gw" {
+#   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "muyiwa-igw-public"
-  }
-}
+#   tags = {
+#     Name = "muyiwa-igw-public"
+#   }
+# }
 
-# Route Table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+# # Route Table
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
 
-  tags = {
-    Name = "muyi-rtw-public"
-  }
-}
+#   tags = {
+#     Name = "muyi-rtw-public"
+#   }
+# }
 
-# Route Table Association
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
+# # Route Table Association
+# resource "aws_route_table_association" "public" {
+#   subnet_id      = aws_subnet.public.id
+#   route_table_id = aws_route_table.public.id
+# }
 
-# Security Group
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
+# # Security Group
+# resource "aws_security_group" "allow_ssh" {
+#   name        = "allow_ssh"
+#   description = "Allow SSH inbound traffic"
+#   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description = "SSH from anywhere"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "SSH from anywhere"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "allow_ssh"
-  }
-}
+#   tags = {
+#     Name = "allow_ssh"
+#   }
+# }
 
-# EC2 Instance
-resource "aws_instance" "web" {
-  ami           = "ami-00bd8b606829fb560" 
-  instance_type = "t3.medium"
-  subnet_id     = aws_subnet.public.id
-  security_groups = [aws_security_group.allow_ssh.id]
+# # EC2 Instance
+# resource "aws_instance" "web" {
+#   ami           = "ami-00bd8b606829fb560" 
+#   instance_type = "t3.medium"
+#   subnet_id     = aws_subnet.public.id
+#   security_groups = [aws_security_group.allow_ssh.id]
   
-  # Key pair name (create this in AWS Console first)
-  key_name = "tos"
+#   # Key pair name (create this in AWS Console first)
+#   key_name = "tos"
 
-  tags = {
-    Name = "web-server"
-  }
-}
+#   tags = {
+#     Name = "web-server"
+#   }
+# }
 
-# Output the public IP
-output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.web.public_ip
-}
+# # Output the public IP
+# output "instance_public_ip" {
+#   description = "Public IP of the EC2 instance"
+#   value       = aws_instance.web.public_ip
+# }
